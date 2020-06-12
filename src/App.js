5da15db92382296c4f2e9cb7fa7280f162734fa8
Azeem Ansari <azeem.ansari870@gmail.com>
2020-06-12 10:27:39 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.png'
 
-function App() {
+
+class App extends React.Component {
+
+  state = {direction : "grow", text: "Breathe In!"}
+  
+   componentDidMount(){
+     this.breathAnimation()
+     setInterval(() => {
+       this.breathAnimation()
+     }, 7500)
+   }
+
+   breatheIn(){
+     this.setState({
+       direction: 'grow',
+       text: 'Breathe In'
+     })
+   }
+
+   breatheHold(){
+     this.setState({
+       text: 'Hold',
+       direction: 'grow'
+     })
+   }
+
+   breatheOut(){
+     this.setState({
+       direction: 'shrink',
+       text: 'Breathe Out'
+     })
+   }
+ 
+  breathAnimation = () => {
+    const totalTime = 7500;
+    const breatheTime = (totalTime / 5) * 2;
+    const holdTime = totalTime / 5;
+    this.breatheIn()
+
+    setTimeout(() => {
+      this.breatheHold()
+
+      setTimeout(() => {
+        this.breatheOut()
+      }, holdTime);
+    }, breatheTime);
+}
+
+  render(){
+    
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <div><img src={Logo} alt="Azeem Ansari" /></div>
+    <h1>Relax</h1>
+    <div className={'container ' + this.state.direction}>
+      <div className="circle"></div>
+
+      <p id="text">{this.state.text.toUpperCase()}</p>
+
+      <div className="pointer-container">
+        <span className="pointer"></span>
+      </div>
+
+      <div className="gradient-circle"></div>
+
+    </div>
     </div>
   );
+  }
 }
 
 export default App;
