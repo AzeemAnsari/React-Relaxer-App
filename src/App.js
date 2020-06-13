0d7d1f4cd9e06dc913e9a3e853f3a8b7c12b37c2
Azeem Ansari <azeem.ansari870@gmail.com>
2020-06-13 22:54:26 +0400

    Start Stop function modified

--- src/App.js
+++ src/App.js
@@ -6,79 +6,93 @@ import Logo from './logo.png';
 
 class App extends React.Component {
 
-  state = {direction : '', text:'', pointer: ''}
+	state = {direction : '',
+					text:'',
+					pointer: '',
+					intervalID: '',
+					toggle: false,
+					clearBH: '',
+					clearBO: ''
+				}
 
-   breatheIn(){
-     this.setState({
-       direction: 'grow',
-       text: 'Breathe In'
-     })
-   }
+	 breatheIn(){
+		 this.setState({
+			 direction: 'grow',
+			 text: 'Breathe In'
+		 })
+	 }
 
-   breatheHold(){
-     this.setState({
-       direction: 'grow',
-       text: 'Hold'
-     })
-   }
+	 breatheHold(){
+		 this.setState({
+			 direction: 'grow',
+			 text: 'Hold'
+		 })
+	 }
 
-   breatheOut(){
-     this.setState({
-       direction: 'shrink',
-       text: 'Breathe Out'
-     })
-   }
+	 breatheOut(){
+		 this.setState({
+			 direction: 'shrink',
+			 text: 'Breathe Out'
+		 })
+	 }
  
-  breathAnimation = () => {
-    const totalTime = 7500;
-    const breatheTime = (totalTime / 5) * 2;
-    const holdTime = totalTime / 5;
-    this.setState({pointer: 'start'})
-    this.breatheIn()
+	breathAnimation = () => {
+		const totalTime = 7500;
+		const breatheTime = (totalTime / 5) * 2;
+		const holdTime = totalTime / 5;
+		this.setState({pointer: 'start'})
+		this.breatheIn()
 
-    setTimeout(() => {
-      this.breatheHold()
+		const bhtimeOut = setTimeout(() => {
+			this.breatheHold()
 
-      setTimeout(() => {
-        this.breatheOut()
-      }, holdTime);
-    }, breatheTime);
-  }
+
+		const botimeOut =	setTimeout(() => {
+				this.breatheOut()		
+			}, holdTime);
+		}, breatheTime);
+		// this.setState({clearBO: botimeOut});
+		this.setState({clearBH: bhtimeOut});
+		
+	}
 
 startNow = () => {
-     this.breathAnimation()
-     const myInterval = setInterval(() => {
-       this.breathAnimation()
-     }, 7500)
-  }
+		this.setState({toggle: true})
+		 this.breathAnimation()
+		 const myInterval = setInterval(() => {
+			 this.breathAnimation()
+		 }, 7500)
+		 this.setState({intervalID: myInterval})
+	}
 
 stopFunction = () => {
-     this.setState({direction : '', text:'', pointer: ''})
-   }
+		clearTimeout(this.state.clearBH);
+		clearInterval(this.state.intervalID);
+		 this.setState({direction : '', text:'', toggle: false, pointer: ''})
+	 }
 
-  render(){
-    
-  return (
-    <div className="main">
-    <div><img src={Logo} alt="Azeem Ansari" /></div>
-    <button type="button" onClick={this.startNow}>Start Now</button>
-    <button type="button" onClick={this.stopFunction}>Stop</button>
-    <h1>Relax</h1>
-    <div className={`container ${this.state.direction}`}>
-      <div className="circle"></div>
+	render(){
+		
+	return (
+		<div className="main">
+		<div className="logo"><img src={Logo} alt="Azeem Ansari" /></div>
+		<h1>Relax</h1>
+		<div className={`container ${this.state.direction}`}>
+			<div className="circle"></div>
 
-      <p>{this.state.text.toUpperCase()}</p>
+			<p>{this.state.text.toUpperCase()}</p>
 
-      <div className={`pointer-container ${this.state.pointer}`}>
-        <span className="pointer"></span>
-      </div>
+			<div className={`pointer-container ${this.state.pointer}`}>
+				<span className="pointer"></span>
+			</div>
 
-      <div className="gradient-circle"></div>
+			<div className="gradient-circle"></div>
 
-    </div>
-    </div>
-  );
-  }
+		</div>
+		{this.state.toggle ? <button type="button" onClick={this.stopFunction}>Stop</button> : <button type="button" onClick={this.startNow}>Start Now</button>}
+		</div>
+	);
+	}
 }
 
 export default App;
